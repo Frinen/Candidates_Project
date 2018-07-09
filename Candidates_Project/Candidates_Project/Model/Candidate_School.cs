97d33cc5b6f46239f95078e41b3f76603036ebbc
Frinen <extremalbp@gmail.com>
2018-07-09 17:32:37 +0300

    DB without connections

---
+++ Candidates_Project/Candidates_Project/Model/Candidate_School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Candidates_Project.Model
+{
+    public class Candidate_School
+    {
+        public int HighSchoolID { get; set; }
+        public int CandidatesID { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public string Degree { get; set; }
+
+    }
+}
