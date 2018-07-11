869fdd3a70dd9cba86ffff1ac1ac0e8c3a31ab57
Frinen <extremalbp@gmail.com>
2018-07-11 13:49:03 +0300

    Change names

--- Candidates_Project/Candidates_Project/Data/DBInitialize.cs
+++ Candidates_Project/Candidates_Project/Data/DBInitialize.cs
@@ -18,7 +18,7 @@ namespace Candidates_Project.Data
                 return;   // DB has been seeded
             }
 
-           /* var person = new Candidate { FirstName = "Fort", LastName = "Carson", BirthDate = DateTime.Parse("1995-09-01"), Sex = "Male", PhoneNumber = "806855058623", Email = "FCarson@gnmail.com", Skype = "FCarson" };
+            /*var person = new Candidate { FirstName = "Fort", LastName = "Carson", BirthDate = DateTime.Parse("1995-09-01"), Sex = "Male", PhoneNumber = "806855058623", Email = "FCarson@gnmail.com", Skype = "FCarson" };
             context.Candidates.Add(person);
 
             var options = new Options { CandidateID = 1, CanRelocate = false, CanWorkInTheOffice = true, CanWorkRemotly = true };
@@ -41,16 +41,16 @@ namespace Candidates_Project.Data
             school = new HighSchool { ID = 2, Name = "BSFEU" };
             context.HighSchools.Add(school);
 
-            var candidate_l = new Candidate_Language { CandidateID = 1, LanguageID = 1, Level = "Upper-Intermediate" };
-            context.Candidate_Language.Add(candidate_l);
-            candidate_l = new Candidate_Language { CandidateID = 1, LanguageID = 3, Level = "Elementary" };
-            context.Candidate_Language.Add(candidate_l);
+            var candidate_l = new CandidateLanguage { CandidateID = 1, LanguageID = 1, Level = "Upper-Intermediate" };
+            context.CandidateLanguages.Add(candidate_l);
+            candidate_l = new CandidateLanguage { CandidateID = 1, LanguageID = 3, Level = "Elementary" };
+            context.CandidateLanguages.Add(candidate_l);
 
-            var candidate_sch = new Candidate_School { HighSchoolID = 1, CandidateID = 1, From = DateTime.Parse("2013-09-01"), To = DateTime.Parse("2018-05-01"), Degree = "Master" };
-            context.Candidate_Schools.Add(candidate_sch);
+            var candidate_sch = new CandidateSchool { HighSchoolID = 1, CandidateID = 1, From = DateTime.Parse("2013-09-01"), To = DateTime.Parse("2018-05-01"), Degree = "Master" };
+            context.CandidateSchools.Add(candidate_sch);
 
-            var candidate_sk = new Candidate_Skill { CandidateID = 1, SkillID = 1, Level = "Master", Month = 24 };
-            context.Candidate_Skills.Add(candidate_sk);
+            var candidate_sk = new CandidateSkill { CandidateID = 1, SkillID = 1, Level = "Master", Month = 24 };
+            context.CandidateSkills.Add(candidate_sk);
             */
             context.SaveChanges();
         }
