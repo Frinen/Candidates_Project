3c6a8283a9ecf3d4885f964a63017cd1b381d904
Frinen <40939021+Frinen@users.noreply.github.com>
2018-07-13 14:03:13 +0300

    Fail controller

--- Candidates_Project/Candidates_Project/Controllers/TestController.cs
+++ Candidates_Project/Candidates_Project/Controllers/TestController.cs
@@ -3,14 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Candidates.Models.Context;
+using Candidates.Services;
 
 namespace Candidates_Project.Controllers
 {
     [Route("api/TestController")]
     public class TestController : Controller
     {
+        private readonly CandidatesContext context;
 
-        // GET: Default
+        public TestController (CandidatesContext _context)
+        {
+            context = _context;
+        }
 
         // GET: Default/Details/5
         [HttpGet]
@@ -31,8 +37,10 @@ namespace Candidates_Project.Controllers
         }
         
         [HttpPost]
-        public ActionResult Add()
+        public ActionResult Add(string firstName, string lastName, string birthDate, string sex, string phoneNumber, string email, string skype)
         {
+            AddEntity.AddCandidate(context, firstName, lastName, birthDate, sex, phoneNumber, email, skype);
+
             return View();
         }
 
