806dabd730c96cde12f4db7256800137ee3ecfaa
Frinen <40939021+Frinen@users.noreply.github.com>
2018-07-13 12:41:30 +0300

    Update controller (again)

--- Candidates_Project/Candidates_Project/Controllers/TestController.cs
+++ Candidates_Project/Candidates_Project/Controllers/TestController.cs
@@ -6,32 +6,30 @@ using System.Threading.Tasks;
 
 namespace Candidates_Project.Controllers
 {
-    
+    [Route("api/TestController")]
     public class TestController : Controller
     {
 
         // GET: Default
 
         // GET: Default/Details/5
-        [Route("api/TestController/Details")]
         [HttpGet]
         public ActionResult Details()
         {
             return View();
         }
-        [Route("api/TestController/Delete")]
         [HttpDelete]
         public ActionResult Delete()
         {
             return View();
         }
-        [Route("api/TestController/Change")]
+        
         [HttpPut]
         public ActionResult Change()
         {
             return View();
         }
-        [Route("api/TestController/Add")]
+        
         [HttpPost]
         public ActionResult Add()
         {
